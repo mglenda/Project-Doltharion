e839dfb10d8a32dfbaee4f406e8e8dc4e85bba82
beckx <mrmglenda@gmail.com>
2023-05-15 17:54:57 +0200

    StringUtils polish

--- Scripting/1_Libraries/Utilities/StringUtils.lua
+++ Scripting/1_Libraries/Utilities/StringUtils.lua
@@ -4,22 +4,10 @@ do
     stringUtils.__index = stringUtils
 
     function stringUtils:round(value,decplaces)
-        local dot,s = StringUtils:instr(tostring(value),'.'),tostring(value)
-        decplaces = decplaces and (not(dot) and decplaces == 0 and decplaces or (dot and decplaces or decplaces + 1)) or 0
-        local decs = decplaces > 0 and (dot and '' or '.') or ''
-        dot = decplaces > 0 and (dot or string.len(s)) or (dot and dot - 1 or string.len(s))
-        for i=1,decplaces do
-            decs = decs .. '0'
-        end
-        return string.sub(s .. decs,1,dot+decplaces)
-    end
-
-    function stringUtils:instr(str,find)
-        for i=1,string.len(str) do
-            if string.sub(str,i,i) == find then
-                return i
-            end
-        end
-        return nil
+        local s,decplaces = tostring(value),decplaces or 0
+        local dp = string.find(s,'.',1,true)
+        s = not(dp) and s .. '.0000' or s .. '0000'
+        dp = dp or string.len(tostring(value)) + 1
+        return string.sub(s,1,decplaces > 0 and dp + decplaces or dp - 1)
     end
 end
